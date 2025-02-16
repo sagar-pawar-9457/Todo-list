a0bdcadb1ca10b32060d4bb026218eacb037a1cd
Sagar Pawar <163662611+sagar-pawar-9457@users.noreply.github.com>
2025-02-16 17:10:32 +0000

    images add

--- app.js
+++
@@ -1,9 +0,0 @@
-const btnEl = document.querySelector(".btn");
-
-btnEl.addEventListener("mouseover",(e)=>{
-    btnEl.computedStyleMap.
-    const x = e.pageX - btnEl.offsetLeft;
-    const Y = e.pageY - btnEl.offsetTop;
-    
-    
-})
